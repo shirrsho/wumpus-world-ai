84c00595d8931cab4c990c0a58c15afb41dfb876
shirsho-linux-home <bsse1106@iit.du.ac.bd>
2022-09-08 15:31:28 +0600

    agent can move on mouseclick

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Board from './Board/board';
 
 function App() {
   return (
     <div className="App">
+      <Board/>
     </div>
   );
 }
---
+++ src/Board/board.js
@@ -0,0 +1,170 @@
+import React, { useEffect, useState } from 'react';
+import './board.css';
+import agent from'./agent.png';
+
+/*
+    Cell classes:
+    =============
+    - unvisited (blurred)
+    - safe
+    - stinky
+    - breeze
+    - agentsafe
+    - agentstinky
+    - agentbreeze
+    - wumpus (blurred)
+    - pit (blurred)
+    - gold (blurred)
+    - agentwumpus
+    - agentpit
+    - agentgold
+*/
+
+const Board = () => {
+	const row_count = 10;
+	const col_count = 10;
+	const [cells, setCells] = useState(Array(row_count*col_count).fill(''));
+    const [agentAddress, setAgentAddress] = useState(27);
+
+	const Cell = ({ num }) => {
+		return <td onClick={() => setAgentAddress(num)}>
+                    <div className={cells[num]}>
+                        {num==agentAddress?
+                            <img src={agent} height={60} width={60}/>:<></>}
+                    </div>
+            </td>;
+	};
+
+    var t = 0;
+
+	return (
+		<div className='container mt-5'>
+			<table className="box">
+				<tbody>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+					<tr>
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+						<Cell num={t++} />
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Board;
